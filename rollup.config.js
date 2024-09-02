9c9227780dae60b8434a6d7117f670a06dc5be56
pepedd864 <pepedd@qq.com>
2024-09-02 16:43:55 +0800

    feat: 调整rollup的打包配置，使用eslint检查

--- rollup.config.js
+++ rollup.config.js
@@ -4,32 +4,38 @@ import babel from '@rollup/plugin-babel'
 import terser from '@rollup/plugin-terser'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import eslint from '@rollup/plugin-eslint'
 
 const config = [
   {
     input: 'src/main.ts',
     output: [
       {
-        file: 'build/index.js',
+        dir: 'build/',
         format: 'esm',
+        preserveModules: true,
       },
     ],
     plugins: [
+      eslint({
+        throwOnError: true,
+      }),
       typescript({
         tsconfig: './tsconfig.json',
       }),
-      nodeResolve(), commonjs(),
+      nodeResolve(),
+      commonjs(),
       terser(),
       babel({
         babelHelpers: 'bundled',
-        extensions: ['.js','.ts'],
+        extensions: ['.js', '.ts'],
       }),
     ],
   },
   {
     input: 'src/main.ts',
-    output: [{ file: 'build/index.d.ts', format: 'es' }],
+    output: [{ dir: 'build/type', format: 'es', preserveModules: true }],
     plugins: [dts()],
   },
 ]
-export default config
+export default config;
